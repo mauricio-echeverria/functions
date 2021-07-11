44bcc2235ed78e74ef068519ab45ce1f791035b2
mauricio.echeverria <ext_mecheverriav@falabella.cl>
2021-07-11 15:10:54 -0400

    ${PROJECTID_GCP}

--- GCP/login-service-account/login.sh
+++ GCP/login-service-account/login.sh
@@ -4,5 +4,6 @@ echo "$GCP_JSON_FILE" > gcloud-service-account.json
 gcloud auth activate-service-account --key-file=gcloud-service-account.json
 ls -la
 cat gcloud-service-account.json
+echo "Login GCP project ${PROJECTID_GCP}"
 gcloud config set project ${PROJECTID_GCP}
-export GOOGLE_APPLICATION_CREDENTIALS=gcloud-service-account.json
+#export GOOGLE_APPLICATION_CREDENTIALS=gcloud-service-account.json
