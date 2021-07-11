b6f2e6eb38fc0e17f17707048ea25f976d1a5a2a
mauricio.echeverria <ext_mecheverriav@falabella.cl>
2021-07-11 15:32:01 -0400

    correccion

--- GCP/login-service-account/login.sh
+++ GCP/login-service-account/login.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Login GCP project ${PROJECTID_GCP}"
-# gcloud config set project ${PROJECTID_GCP}
-echo "Login Service Account GCP"
-# echo "$GCP_JSON_FILE" > gcloud-service-account.json
-# gcloud auth activate-service-account --key-file=gcloud-service-account.json
+echo "*************************************************"
+gcloud config set project ${PROJECTID_GCP}
+echo "Login Service Account GCP project ${PROJECTID_GCP}"
+echo "$GCP_JSON_FILE" > gcloud-service-account.json
+gcloud auth activate-service-account --key-file=gcloud-service-account.json
 #export GOOGLE_APPLICATION_CREDENTIALS=gcloud-service-account.json
